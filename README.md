6e472e7bf3eb2fe0135eea6d36300bffb46d476b
ME.AT <meatish2008@gmail.com>
2024-12-30 20:04:33 +0330

    update and refactor code

--- README.md
+++
@@ -1,4 +0,0 @@
-This is a password generator.
-When you need password and need password you can use this.
-In this project, I use big and small characters and Special sign (like : @ , _ , - , & ,...) and number.
-All this select random characters and always not similar.
--- server.js
+++ server.js
@@ -2,109 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3006;
 
-const alphabets = {
-  bigCharacters: {
-    bigChar: [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ],
-  },
-  smallCharacters: {
-    smallChar: [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ],
-  },
-};
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const specialSign = ["@", "#", "$", "*", "&", "_", "/", ".", "|", "?"];
+const mainGenerator = async () => {
+  const randomNumber = Math.floor(Math.random() * (14 - 8)) + 8;
+  const exceptions = [
+    34, 39, 40, 41, 44, 43, 45, 47, 57, 58, 59, 60, 61, 62, 91, 92, 93, 94, 96,
+  ];
 
-const mainGenerator = async (alphabets, numbers, specialSign) => {
-  let password = null;
-  const randomIndex = Math.floor(Math.abs(Math.random() * 10)).toString();
-  const randomIndexTwo = Math.floor(Math.abs(Math.random() * 5));
-  const randomIndexAlphabets = Math.floor(Math.abs(Math.random() * 26));
-  const randomIndexAlphabetsTwo = Math.floor(Math.abs(Math.random() * 13));
-
-  let i = Math.floor(Math.abs(Math.random() * (14 - 8 + 1) + 8));
-  for (i; i > 0; i--) {
-    let randomNumber = numbers[randomIndex];
-    let randomSpecialSign = specialSign[randomIndex];
-    let randomSmallAlphabets =
-      alphabets.smallCharacters.smallChar[randomIndexAlphabets];
-    let randomBigAlphabets =
-      alphabets.bigCharacters.bigChar[randomIndexAlphabets];
-    password = randomBigAlphabets;
-    password += randomNumber;
-    password += randomSpecialSign;
-    password += randomSmallAlphabets;
-
-    let randomNumber2 = numbers[randomIndexTwo];
-    let randomSpecialSign2 = specialSign[randomIndexTwo];
-    let randomSmallAlphabets2 =
-      alphabets.smallCharacters.smallChar[randomIndexAlphabetsTwo];
-    let randomBigAlphabets2 =
-      alphabets.bigCharacters.bigChar[randomIndexAlphabetsTwo];
-    password += randomSpecialSign2;
-    password += randomBigAlphabets2;
-    password += randomNumber2;
-    password += randomSmallAlphabets2;
-  }
+  const char = Array.from(
+    { length: randomNumber }, // create array with random slot
+    () => {
+      let asciiCode;
+      do {
+        asciiCode = Math.floor(Math.random() * (122 - 35 + 1)) + 35; // create random ASCII code between 35 to 122
+      } while (exceptions.includes(asciiCode)); // exclude the exception number generated
+      return asciiCode;
+    }
+  );
+  let password = Buffer.from(char).toString(); // convert ascii code to string
   return password;
 };
-mainGenerator(alphabets, numbers, specialSign);
 
-app.get("/passwordGenerator", async (req, res) => {
-  password = await mainGenerator(alphabets, numbers, specialSign);
-  res.send(password)
+app.get("/passwordGenerator",  async (req, res) => {
+  password = await mainGenerator()
+  res.send(password);
 });
 
 app.listen(PORT || 3006, () => {
