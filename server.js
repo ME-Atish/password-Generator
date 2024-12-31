30cd8904f94b06f875cb4ea9613160e8d0cce556
ME.AT <meatish2008@gmail.com>
2024-12-31 21:09:07 +0330

    add some arr

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const mainGenerator = async () => {
   const exceptions = [
     34, 39, 40, 41, 44, 43, 45, 47, 57, 58, 59, 60, 61, 62, 91, 92, 93, 94, 96,
   ];
-
+  const specialSigns = [35, 36, 37, 38, 42, 46, 63, 64];
+  const numbers = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+  const smallChars = [
+    97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
+    113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+  ];
+  const bigChars = [
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+    84, 85, 86, 87, 88, 89, 90,
+  ];
   const char = Array.from(
     { length: randomNumber }, // create array with random slot
     () => {
