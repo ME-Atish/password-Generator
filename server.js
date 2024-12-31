fd62164e8111af5f4062c4e40e4973c7505575e7
ME.AT <meatish2008@gmail.com>
2024-12-31 20:47:35 +0330

    delete new arr

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 3006;
 
-const specialSigns = [35, 36, 37, 38, 42, 46, 63, 64];
-const numbers = [48 , 49 , 50 ,51 , 52 , 53 , 54 , 55 , 56 , 57]
-const smallChars = [97 , 98 , 99 , 100 , 101 , 102 , 103 , 104 , 105 , 106 ,107 , 108 , 109 , 110 , 111 , 112 , 113 , 114 , 115 , 116 , 117 , 118 , 119 , 120 , 121 , 122]
-const bigChars = [65 , 66 , 67 , 68 , 69 , 70 , 71 , 72 ,73 , 74 , 75 , 76 , 77 , 78 , 79 , 80 , 81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90]
-console.log(smallChars.length);
-
 const mainGenerator = async () => {
   const randomNumber = Math.floor(Math.random() * (14 - 8)) + 8;
   const exceptions = [
